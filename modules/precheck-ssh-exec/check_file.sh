1f80fe071dace1e51b58e3f8acba2e5b31a7e4a0
Patrick Brennan <patrickabrennan@gmail.com>
2025-05-20 10:22:55 -0400

    Update check_file.sh

--- modules/precheck-ssh-exec/check_file.sh
+++ modules/precheck-ssh-exec/check_file.sh
@@ -1,9 +1,9 @@
-##!/bin/bash
+#!/bin/bash
 exit 0 
-#for FILE in ${@}
-#do
-#  if [[ ! -f $FILE ]]
-#  then
-#    echo -e "ERROR! \n The PATH ${FILE} does not exist!"
-#  fi
-#done
+for FILE in ${@}
+do
+  if [[ ! -f $FILE ]]
+  then
+    echo -e "ERROR! \n The PATH ${FILE} does not exist!"
+  fi
+done
